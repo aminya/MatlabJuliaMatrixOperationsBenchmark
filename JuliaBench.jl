5baa0f6c2d10930b29adf4c23494ad80d66197f8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2019-06-10 15:32:47 -0500

    Fixing Kmeans

--- JuliaBench.jl
+++ JuliaBench.jl
@@ -1,10 +1,8 @@
 
 function JuliaBench(operationMode)
 
-    allFunctions = [MatrixGeneration, MatrixAddition, MatrixMultiplication, MatrixQuadraticForm, MatrixReductions, ElementWiseOperations, MatrixExp, MatrixSqrt, Svd, Eig, CholDec, MatInv, LinearSystem, LeastSquares];
+    allFunctions = [MatrixGeneration, MatrixAddition, MatrixMultiplication, MatrixQuadraticForm, MatrixReductions, ElementWiseOperations, MatrixExp, MatrixSqrt, Svd, Eig, CholDec, MatInv, LinearSystem, LeastSquares, CalcDistanceMatrix, KMeans];
 
-    # need tweaking:
-    # , CalcDistanceMatrix, KMeans
 
     allFunctionsString = ["Matrix Generation", "Matrix Addition", "Matrix Multiplication", "Matrix Quadratic Form", "Matrix Reductions", "Element Wise Operations", "Matrix Exponential", "Matrix Square Root", "Singular Value Decomposition", "Eigen Decomposition","Cholesky Decomposition", "Matrix Inversion", "Linear System Solution", "Linear Least Squares", "Squared Distance Matrix", "K-Means"];
 
@@ -34,9 +32,11 @@ function JuliaBench(operationMode)
             println("Processing $(allFunctionsString[jj]) - MatrixSize= $matrixSize");
 
             for kk = 1:numIterations
-
-                benchijk =@benchmark $fun($matrixSize, $mX, $mY);
-                mRunTime[ii, jj, kk]=minimum(benchijk.times);
+                benchmarkableIJK =@benchmarkable $fun($matrixSize, $mX, $mY);
+                tune!(benchmarkableIJK)
+                benchmarkableIJK
+                # benchIJK =@benchmark $fun($matrixSize, $mX, $mY);
+                # mRunTime[ii, jj, kk]=minimum(benchijk.times);
 
             end
             jj+=1;
@@ -112,41 +112,41 @@ end
 
 function MatrixExp( matrixSize, mX, mY )
 
-  mA = exp(mX);
+    mA = exp(mX);
 
-  return mA;
+    return mA;
 end
 
 function MatrixSqrt( matrixSize, mX, mY  )
 
-  mY = transpose(mX) * mX;
+    mY = transpose(mX) * mX;
 
-  mA = sqrt(mY);
+    mA = sqrt(mY);
 
-  return mA;
+    return mA;
 end
 
 function Svd( matrixSize, mX, mY  )
 
-  F = svd(mX, full = false);
+    F = svd(mX, full = false);
 
-  return mA=0;
+    return mA=0;
 end
 
 function Eig( matrixSize, mX, mY  )
 
-  mE = eigvals(mX);
+    mE = eigvals(mX);
 
-  return mA=0;
+    return mA=0;
 end
 
 function CholDec( matrixSize, mX, mY  )
 
-  mY = transpose(mX) * mX;
+    mY = transpose(mX) * mX;
 
-  mA = cholesky(mY);
+    mA = cholesky(mY);
 
-  return mA;
+    return mA;
 end
 
 function MatInv( matrixSize, mX, mY  )
@@ -158,65 +158,64 @@ function MatInv( matrixSize, mX, mY  )
 
     mA = mA .+ mB;
 
-  return mA;
+    return mA;
 end
 
 
 function LinearSystem( matrixSize, mX, mY  )
 
-  mB = randn(matrixSize, matrixSize);
-  vB = randn(matrixSize);
+    mB = randn(matrixSize, matrixSize);
+    vB = randn(matrixSize);
 
-  vA = mX \ vB;
-  mA = mX \ mB;
+    vA = mX \ vB;
+    mA = mX \ mB;
 
-  mA = mA .+ vA;
+    mA = mA .+ vA;
 
-  return mA;
+    return mA;
 end
 
 function LeastSquares( matrixSize, mX, mY  )
 
-  mB = randn(matrixSize, matrixSize);
-  vB = randn(matrixSize);
+    mB = randn(matrixSize, matrixSize);
+    vB = randn(matrixSize);
 
-  mXT=transpose(mX);
-  vA = ( mXT * mX) \ ( mXT * vB);
-  mA = ( mXT * mX) \ ( mXT * mB);
+    mXT=transpose(mX);
+    vA = ( mXT * mX) \ ( mXT * vB);
+    mA = ( mXT * mX) \ ( mXT * mB);
 
-  mA = mA .+ vA;
+    mA = mA .+ vA;
 
-  return mA;
+    return mA;
 end
 
 function CalcDistanceMatrix( matrixSize, mX, mY  )
 
-  mY = randn(matrixSize, matrixSize);
+    mY = randn(matrixSize, matrixSize);
 
-  mA = transpose( sum(mX .^ 2) ) .- (2 .* transpose(mX) * mY) .+ sum(mY .^ 2);
-# ????? sum(,1)
-  return mA;
+    mA = transpose( sum(mX .^ 2, dims=1) ) .- (2 .* transpose(mX) * mY) .+ sum(mY .^ 2, dims=1);
+
+    return mA;
 end
 
 function KMeans( matrixSize, mX, mY  )
 
-  # Assuming Samples are slong Columns (Rows are features)
-  numClusters     = Int64(max(round(matrixSize / 100), 1));
-  vClusterId      = zeros(matrixSize);
-  numIterations   = 100;
+    # Assuming Samples are slong Columns (Rows are features)
+    numClusters     = Int64(max(round(matrixSize / 100), 1));
+    vClusterId      = zeros(matrixSize);
+    numIterations   = 100;
 
-  # http://stackoverflow.com/questions/36047516/julia-generating-unique-random-integer-array
-  mA          = mX[:, randperm(matrixSize)[1:numClusters]]; #<! Cluster Centroids
+    # http://stackoverflow.com/questions/36047516/julia-generating-unique-random-integer-array
+    mA          = mX[:, randperm(matrixSize)[1:numClusters]]; #<! Cluster Centroids
 
-  for ii = 1:numIterations
-    vMinDist, vClusterId[:] = findmin(transpose(sum(mA .^ 2)) .- (2 .* transpose(mA)* mX), 1); #<! Is there a `~` equivalent in Julia?
-    # ????? sum(,1)
-    for jj = 1:numClusters
-      mA[:, jj] = sum(mX[:, vClusterId .== jj], 2) ./ matrixSize;
+    for ii = 1:numIterations
+        vMinDist, vClusterId[:] = findmin(transpose(sum(mA .^ 2, dims=1)) .- (2 .* transpose(mA)* mX), dims=1); #<! Is there a `~` equivalent in Julia?
+        for jj = 1:numClusters
+            mA[:, jj] = sum(mX[:, vClusterId .== jj], dims=2) ./ matrixSize;
+        end
     end
-  end
 
-  mA = mA[:, 1] .+ transpose(mA[:, end]);
+    mA = mA[:, 1] .+ transpose(mA[:, end]);
 
-  return mA;
+    return mA;
 end
